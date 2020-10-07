7736b1a8b3c0e24f9eba2f75f5c87db2bbe6b099
frgt10cs <ivan_gavrilov_2016@bk.ru>
2020-10-07 23:29:01 +0300

    added juser search

---
+++ MaelstormAPI/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MaelstormDTO.Responses;
+
+namespace MaelstormApi
+{
+    public static class User
+    {
+        public static async Task<List<UserInfo>> FindUsersByNickname(string nickname)
+        {
+            var message = new HttpRequestMessage(HttpMethod.Get,$"users?query={nickname}");
+            var response = await Client.AuthRequestAsync(message);
+            return response.GetContent<List<UserInfo>>();
+        }
+    }
+}
